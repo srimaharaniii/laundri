ff9174ab409805fa2f2055bae023b2cb13c1d4de
putrafahrezi <putrafahrezi89@gmail.com>
2025-07-09 17:17:47 +0700

    memperbaiki halaman index

--- resources/views/penyerahan/index.blade.php
+++ resources/views/penyerahan/index.blade.php
@@ -15,7 +15,7 @@
             <a href="/penyerahan/tambah" class="btn btn-primary btn-sm"><i class="fa fa-user-plus"></i> Tambah Data</a>
         </h3>
 
-        <div class="card-tools">
+                   <div class="card-tools">
         <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
             <i class="fas fa-minus"></i>
         </button>
@@ -26,35 +26,35 @@
     </div>
     <div class="card-body">
         <table class="table">
-                        <thead>
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Pelanggan</th>
+                        <th scope="col">Jenis</th>
+                        <th scope="col">Tgl_Penyerahan</th>
+                        <th scope="col">Tgl_Selesai</th>
+                        <th scope="col">Tgl_Pengambilan</th>
+                        <th scope="col">Harga</th>
+                        <th scope="col">Berat</th>
+                        <th scope="col">Action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ( $penyerahan as $data )
                             <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Pelanggan</th>
-                                <th scope="col">Jenis</th>
-                                <th scope="col">Tgl Penyerahan</th>
-                                <th scope="col">Tgl Selesai</th>
-                                <th scope="col">Tgl Pengambilan</th>
-                                <th scope="col">Harga</th>
-                                <th scope="col">Berat</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @forelse ( $penyerahan as $data)
-                                <tr>
-                                    <th scope="row">{{$nomor++}}</th>
-                                    <td>{{$data->pelanggan}}</td>
-                                    <td>{{$data->jenis}}</td>
-                                    <td>{{$data->tgl_penyerahan}}</td>
-                                    <td>{{$data->tgl_selesai}}</td>
-                                    <td>{{$data->tgl_pengambilan}}</td>
-                                    <td>{{$data->harga}}</td>
-                                    <td>{{$data->berat}}</td>
-                                    <td>
-                                        <a href="" class="btn btn-warning btn-sm"><i class="fa fa-info"></i></a>
+                                <th scope="row">{{ $nomor++ }}</th>
+                                <td>{{$data->pelanggan}}</td>
+                                <td>{{$data->jenis}}</td>
+                                <td>{{$data->tgl_penyelesaian}}</td>
+                                <td>{{$data->tgl_selesai}}</td>
+                                <td>{{$data->tgl_pengambilan}}</td>
+                                <td>{{$data->harga}}</td>
+                                <td>{{$data->berat}}</td>
+                                <td>
+                                    <a href="" class="btn btn-warning btn-sm"><i class="fa fa-info"></i></a>
                                         <a href="/penyerahan/edit/{{$data->id}}" class="btn btn-info btn-sm"><i class="fa fa-pen"></i></a>
 
-
-                                        <!-- Button trigger modal -->
+                                    <!-- Button trigger modal -->
                                         <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal{{$data->id}}">
                                         <i class="fa fa-trash"></i>
                                         </button>
@@ -68,31 +68,32 @@
                                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                             </div>
                                             <div class="modal-body">
-                                                Yakin Data Penyerahan a.n. {{$data->nama}} ingin dihapus?
+                                                Yakin Data penyerahan a.n. {{$data->nama}} ingin dihapus?
                                             </div>
                                             <div class="modal-footer">
                                                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                                                <form action="penyerahan/{{$data->id}}" method="post">
+                                                <form action="penyeeahan/{{$data->id}}" method="post">
                                                     @csrf
                                                     @method('DELETE')
                                                     <button type="submit" class="btn btn-danger">Hapus</button>
                                                 </form>
+                                </td>
 
-                                            </div>
-                                            </div>
-                                        </div>
-                                        </div>
-                                    </td>
-                                </tr>
-                            @empty
-                                <tr>
-                                    <th colspan="5" scope="row">Data Tidak Ada</th>
-                                </tr>
-                            @endforelse
+                            </tr>
 
+                        @empty
+                            <tr>
+                                <th colspan="5" scope="row">Data Tidak Ada</th>
+                            </tr>
 
-                        </tbody>
-                    </table>
+                        @endforelse
+
+                    </tbody>
+                  </table>
+
+                </div>
+            </div>
+        </div>
     </div>
-
+</div>
 @endsection
