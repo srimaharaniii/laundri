03f8ab8fbd26005d56f3bb0e9b36ce43c65e5b59
putrafahrezi <putrafahrezi89@gmail.com>
2025-07-11 13:13:16 +0700

    perbaikan form pelanggan

--- resources/views/pelanggan/form.blade.php
+++ resources/views/pelanggan/form.blade.php
@@ -14,12 +14,12 @@
                         <form action="/pelanggan" method="post" enctype="multipart/form-data">
                             @csrf
                             <div class="mb-3">
-                                <label for="exampleInputEmail1" class="form-label">nm_pelanggan</label>
-                                <input type="text" name="nama" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                <label for="exampleInputEmail1" class="form-label">nama pelanggan</label>
+                                <input type="text" name="nm_pelanggan" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                             </div>
                             <div class="mb-3">
                                 <label for="exampleInputPassword1" class="form-label">alamat</label>
-                                <input type="text" name="alamat" class="form-control" id="exampleInputPassword1">
+                                <input type="text" name="almt_pelanggan" class="form-control" id="exampleInputPassword1">
                             </div>
                             <div class="mb-3">
                                 <label for="exampleInputPassword1" class="form-label">no_hp</label>
